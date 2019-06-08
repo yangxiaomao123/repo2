3e9432e7fd171e6aff1907606aa75b60873ffe87
root <root@123.com>
2019-06-08 21:34:50 +0800

    dsss

---
+++ gitTest/src/main/java/cn/itcast/GitTest.java
@@ -0,0 +1,17 @@
+package cn.itcast;
+
+/**
+ * @author yangx
+ * @date 2019/6/8
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+
+}
